e93ebd10ca49d3c218373c4698ff096db47207dd
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-07-20 20:43:50 -0300

    Inicializando projeto geotools

---
+++ src/main/java/com/starking/geotools/GeotoolsApplication.java
@@ -0,0 +1,13 @@
+package com.starking.geotools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeotoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeotoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starking/geotools/GeotoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starking.geotools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeotoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
